4e4ab3c270cefbaac2ad4a2d5f64c3bf92b84e21
Aaron Clark <Aaron Clark@corp.reinke.com>
2019-07-09 09:16:15 -0500

    First Commit

---
+++ PriceDeterminator.cs
@@ -0,0 +1,129 @@
+﻿namespace CarPricer
+{
+    public class PriceDeterminator
+    {
+        public decimal DetermineCarPrice(Car car)
+        {
+            decimal price = car.PurchaseValue;
+
+            if (HasPreviousOwners(car.NumberOfPreviousOwners)){
+
+                price -= GetAgePenalty(car.AgeInMonths, price);
+                price -= GetMilesPenalty(car.NumberOfMiles, price);
+                price -= GetOwnersPenalty(car.NumberOfPreviousOwners, price);
+                price -= GetCollisionPenalty(car.NumberOfCollisions, price);
+                return price;
+
+            }
+
+            price -= GetAgePenalty(car.AgeInMonths, price);
+            price -= GetMilesPenalty(car.NumberOfMiles, price);
+            price -= GetCollisionPenalty(car.NumberOfCollisions, price);
+            price += GetOwnersBonus(price);
+            return price;
+
+        }
+
+        private bool HasPreviousOwners(int NumberOfPreviousOwners)
+        {
+            if (NumberOfPreviousOwners > 0)
+                return true;
+
+            return false;
+        }
+
+        private decimal GetAgePenalty(int ageInMonths, decimal price)
+        {
+            decimal penalty = 0;
+
+            if (ageInMonths < 360)
+            {
+                for (int i = 0; i < ageInMonths; i++)
+                {
+                    penalty += (price * .005m);
+                }
+
+                return penalty;
+
+            } 
+
+            for (int i = 0; i < 360; i++)
+            {
+                penalty += (price * .005m);
+            }
+
+            return penalty;
+            
+        }
+
+        private decimal GetMilesPenalty(int numberOfMiles, decimal price)
+        {
+            decimal penalty = 0;
+
+            if (numberOfMiles < 150000)
+            {
+                decimal remainder = numberOfMiles % 1000;
+                decimal milesToCalculate = numberOfMiles - remainder;
+                decimal iterations = milesToCalculate / 1000;
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    penalty += (price * .002m);
+
+                }
+
+                return penalty;
+
+            } 
+
+            for (int i = 0; i < 150; i++)
+            {
+                penalty += (price * .002m);
+
+            }
+
+            return penalty;
+
+        }
+
+        private decimal GetOwnersPenalty(int numberOfOwners, decimal price)
+        {
+
+            if (numberOfOwners > 2)
+                return (price * .25m);
+
+            return 0;
+
+        }
+
+        private decimal GetCollisionPenalty(int numberOfCollisions, decimal price)
+        {
+            int maxCollisions = 5;
+            decimal penalty = 0;
+
+            if (numberOfCollisions < maxCollisions)
+            {
+                for (int i = 0; i < (numberOfCollisions); i++)
+                {
+                    penalty += (price * .02m);
+                }
+
+                return penalty;
+
+            }
+
+            for (int i = 0; i < (maxCollisions); i++)
+            {
+                penalty += (price * .02m);
+            }
+
+            return penalty;
+
+        }
+
+        private decimal GetOwnersBonus(decimal price)
+        {
+            return price * .10m;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPricer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           AssertCarValue(25313.40m, 35000m, 3 * 12, 50000, 1, 1);
+           AssertCarValue(19688.20m, 35000m, 3 * 12, 150000, 1, 1);
+           AssertCarValue(19688.20m, 35000m, 3 * 12, 250000, 1, 1);
+           AssertCarValue(20090.00m, 35000m, 3 * 12, 250000, 1, 0);
+           AssertCarValue(21657.02m, 35000m, 3 * 12, 250000, 0, 1);
+        }
+
+        private static void AssertCarValue(decimal expectValue, decimal purchaseValue,
+            int ageInMonths, int numberOfMiles, int numberOfPreviousOwners, int
+                numberOfCollisions)
+        {
+            Car car = new Car
+            {
+                AgeInMonths = ageInMonths,
+                NumberOfCollisions = numberOfCollisions,
+                NumberOfMiles = numberOfMiles,
+                NumberOfPreviousOwners = numberOfPreviousOwners,
+                PurchaseValue = purchaseValue
+            };
+            PriceDeterminator priceDeterminator = new PriceDeterminator();
+            var carPrice = priceDeterminator.DetermineCarPrice(car);
+        }
+    }
+}
